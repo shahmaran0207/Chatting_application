bc1fa7053d855ce74adf973c024f77a5e5067459
shahmaran0207 <shahmaran0207@naver.com>
2024-11-13 14:20:51 +0900

    frontend setting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 import socket from "./server";
+import {useEffect} from "react";
 
 function App() {
+
+    const askUserName=()=>{
+        useEffect(() => {
+            askUserName();
+        }, []);
+
+        const userName=prompt("당신의 이름을 입력해 주세요");
+
+        socket.emit("login", userName, (res)=>{
+            console.log(res);
+        });
+    };
+
   return (
     <div>
       <div className="App"></div>
