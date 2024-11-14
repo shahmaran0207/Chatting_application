735e02456d294e47fd309e2b6260d28b166adf47
shahmaran0207 <shahmaran0207@naver.com>
2024-11-14 13:19:34 +0900

    restart

--- app.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-const mongoURI = process.env.MONGODB_URI;
-
-mongoose.connect(mongoURI)
-    .then(() => console.log('MongoDB 연결 성공'))
-    .catch((err) => console.error('MongoDB 연결 실패:', err));
--- src/server.js
+++
@@ -1,4 +0,0 @@
-import {io} from "socket.io-client";
-const socket = io("http://localhost:5003");
-
-export default socket;
